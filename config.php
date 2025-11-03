d543d4a42aac42460677aece08d4de323d843272
Farzin Hamzehi <5farzinhamzei@gmail.com>
2025-11-03 05:32:17 +0330

    Add config.php for add config of web services and tokens

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+// توکن معتبر برای درخواست (مثلاً برای کنترل دسترسی)
+define('VALID_TOKEN' , "12345")
+
+// کلید رایگان API از سایت OpenWeatherMap
+define('OPENWEATHER_API_KEY', 'your_api_key_here');
+
+// URL پایه‌ی وب‌سرویس
+define('OPENWEATHER_URL', 'https://api.openweathermap.org/data/2.5/weather')
